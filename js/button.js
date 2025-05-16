cf96d95b9fdce233f4fb67262a0e8b2ab64cb051
Luka Jurkic <lukajurkic1@gmail.com>
2025-05-16 17:03:56 +0200

    implement column mapping for every column

--- js/button.js
+++ js/button.js
@@ -1,7 +1,86 @@
 // button.js
+const columnMapping = {
+  chrstprot: "Christianity Protestants",
+  chrstcat: "Christianity Roman Catholics",
+  chrstorth: "Christianity Eastern Orthodox",
+  chrstang: "Christianity Anglican",
+  chrstothr: "Christianity Others",
+  chrstgen: "Christianity Total",
+  judorth: "Judaism Orthodox",
+  jdcons: "Judaism Conservatives",
+  judref: "Judaism Reform",
+  judothr: "Judaism Others",
+  judgen: "Judaism Total",
+  islmsun: "Islam Sunni",
+  islmshi: "Islam Shia",
+  islmibd: "Islam Ibadi",
+  islmnat: "Islam Nation of Islam",
+  islmalw: "Islam Alawite",
+  islmahm: "Islam Ahmadiyya",
+  islmothr: "Islam Other",
+  islmgen: "Islam Total",
+  budmah: "Buddhism Mahayana",
+  budthr: "Buddhism Theravada",
+  budothr: "Buddhism Other",
+  budgen: "Buddhism Total",
+  zorogen: "Zoroastrian Total",
+  hindgen: "Hindu Total",
+  sikhgen: "Sikh Total",
+  shntgen: "Shinto Total",
+  bahgen: "Baha'i Total",
+  taogen: "Taoism Total",
+  jaingen: "Jain Total",
+  confgen: "Confucianism Total",
+  syncgen: "Syncretic Religions Total",
+  anmgen: "Animist Religions Total",
+  nonrelig: "Non Religious Total",
+  othrgen: "Other Religions Total",
+  sumrelig: "Percent Religions Adherents",
+  pop: "Total population in independent states",
+  worldpop: "Total world population",
+  chrstprotpct: "Christianity Protestants Percentage",
+  chrstcatpct: "Christianity Roman Catholics Percentage",
+  chrstorthpct: "Christianity Eastern Orthodox Percentage",
+  chrstangpct: "Christianity Anglican Percentage",
+  chrstothrpct: "Christianity Others Percentage",
+  chrstgenpct: "Christianity Total Percentage",
+  judorthpct: "Judaism Orthodox Percentage",
+  judconspct: "Judaism Conservatives Percentage",
+  judrefpct: "Judaism Reform Percentage",
+  judothrpct: "Judaism Others Percentage",
+  judgenpct: "Judaism Total Percentage",
+  islmsunpct: "Islam Sunni Percentage",
+  islmshipct: "Islam Shia Percentage",
+  islmibdpct: "Islam Ibadi Percentage",
+  islmnatpct: "Islam Nation of Islam Percentage",
+  islmalwpct: "Islam Alawite Percentage",
+  islmahmpct: "Islam Ahmadiyya Percentage",
+  islmothrpct: "Islam Other Percentage",
+  islmgenpct: "Islam Total Percentage",
+  budmahpct: "Buddhism Mahayana Percentage",
+  budthrpct: "Buddhism Theravada Percentage",
+  budothrpct: "Buddhism Other Percentage",
+  budgenpct: "Buddhism Total Percentage",
+  zorogenpct: "Zoroastrian Percentage",
+  hindgenpct: "Hindu Percentage",
+  sikhgenpct: "Sikh Percentage",
+  shntgenpct: "Shinto Percentage",
+  bahgenpct: "Baha'i Percentage",
+  taogenpct: "Taoism Percentage",
+  jaingenpct: "Jain Percentage",
+  confgenpct: "Confucianism Percentage",
+  syncgenpct: "Syncretic Religions Percentage",
+  anmgenpct: "Animist Religions Percentage",
+  nonreligpct: "Non Religious Percentage",
+  othrgenpct: "Other Religions Percentage",
+  sumreligpct: "Percent Religions Adherents",
+  ptctotal: "Percent of total population"
+};
+
 let data = [];
 try {
   data = await d3.csv("data/WRP_global.csv");
+  console.log("CSV columns:", Object.keys(data[0])); // Log columns for verification
   console.log(data);
   data.forEach(row => {
     console.log(row.year, row.chrstprot, row.chrstcat);
@@ -33,9 +112,10 @@ document.getElementById("show-data-btn").addEventListener("click", (event) => {
                  <th>Value</th>
                </tr>`;
     entries.forEach(([key, value]) => {
+      const fullName = columnMapping[key] || key; // Use full name or fallback
       const formattedValue = isNaN(value.replace(/,/g, '')) ? value : parseInt(value.replace(/,/g, '')).toLocaleString();
       html += `<tr>
-                 <td>${key}</td>
+                 <td>${fullName}</td>
                  <td>${formattedValue}</td>
                </tr>`;
     });
@@ -46,7 +126,6 @@ document.getElementById("show-data-btn").addEventListener("click", (event) => {
     .style("display", "block");
   container.classed("blurred", true);
 
-  // Add close button event
   d3.select("#popup .close-btn").on("click", () => {
     popup.style("display", "none");
     container.classed("blurred", false);
